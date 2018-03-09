75f21c893cc4beee036c771d61a8d972433ffd7e
Mikael B. Kiste <mikael.kiste@gmail.com>
2018-03-09 19:09:40 +0100

    almost done

--- module2/plot.py
+++ module2/plot.py
@@ -1,8 +1,21 @@
 from numpy import *
 from matplotlib.pyplot import *
 
-N=int(1e4)
+N=int(1e2)
 x = arange(N)+1
 
+def dos(w,w0):
+	return N/(pi*sqrt(w0**2-w**2))
+
+figure()
+plot(x,dos(x,N))
+xlabel("$\omega$")
+ylabel("DOS")
+savefig("2b.pdf")
+
+figure()
 plot(x,ones(N)/(2*pi))
-show()
+xlabel("k")
+ylabel("DOS[L]",rotation=0)
+grid()
+savefig("2a.pdf")
