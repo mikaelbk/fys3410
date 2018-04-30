9e31bb5f109832c45971c13e3e3f3d56bb2bf16d
Mikael B. Kiste <mikael.kiste@gmail.com>
2018-04-30 17:43:42 +0200

    I am so late for this...got some programming in today

---
+++ module3/alkaliValues.py
@@ -0,0 +1,45 @@
+from numpy import *
+from matplotlib.pyplot import *
+from scipy import constants
+
+
+hbar = 1.0545718E-34 # Plancks reduced constant
+m = 9.10938356E-31 # electron mass
+kb = 1.38064852E-23 # Boltzmann constant
+q = constants.e # elementary charge
+
+def e(k):
+	return (k*hbar)**2/(2*m)
+
+def k(c):
+	return (3*pi**2*c)**(1/3.0)
+
+def v(k):
+	return hbar*k/m
+
+def t(e):
+	return e/kb
+
+c = 4.7E22*(1E6)
+print(k(c)*1E-8*1E-2)
+print(e(k(c))/q)
+print(v(k(c))*1E-8*1E2)
+
+elCon = 1E28*array( [4.7,2.65,1.4,1.15,0.91] )
+
+subplot(2,2,1)
+plot(k(elCon),'bo')
+plot(k(elCon))
+
+subplot(2,2,2)
+plot(e(k(elCon)),'bo')
+plot(e(k(elCon)))
+
+subplot(2,2,3)
+plot(v(k(elCon)),'bo')
+plot(v(k(elCon)))
+
+subplot(2,2,4)
+plot(t(e(k(elCon))),'bo')
+plot(t(e(k(elCon))))
+show()
